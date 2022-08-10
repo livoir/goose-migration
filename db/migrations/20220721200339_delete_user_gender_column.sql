2e2522173fde4c1f0b8af5ac55346bc9c3ca9720
Iqbal Alfikri <iqbalalfikri12@gmail.com>
2022-08-10 17:44:16 +0700

    test

--- db/migrations/20220721200339_delete_user_gender_column.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE users drop column gender;
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-SELECT 'down SQL query';
--- +goose StatementEnd
